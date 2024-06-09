ba2c9638ffd9a19d9fdeb27f1d6aad1e5f4f34d3
Jarhatz <rajathrao1001@gmail.com>
2024-06-09 15:48:28 -0700

    im hungry

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -41,6 +41,38 @@ function Experience() {
             </div>
           </div>
         </div>
+        <div className="exp-comp2a exp-comp2b">
+          <p className="exp-header">Work</p>
+          <div className="exp-content-comp">
+            <div className="exp-content">
+              <p className="exp-text1">
+                <span className="emphasize emphasize3">
+                  M.S. in Data Science
+                </span>{" "}
+                • December 2024
+              </p>
+              <p className="exp-text2">
+                Stony Brook University | Stony Brook, NY
+              </p>
+              <p className="exp-text1">
+                Deep Learning research with Professor Andrew Schwartz in
+                multimodal models and Brain Computer Interfaces leveraging
+                NLP/CV deep learning paradigms.
+              </p>
+            </div>
+            <div className="exp-content">
+              <p className="exp-text1">
+                <span className="emphasize emphasize3">
+                  B.S. in Computer Science & Engineering
+                </span>{" "}
+                • March 2023
+              </p>
+              <p className="exp-text2">
+                University of California, Irvine | Irvine, CA
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
