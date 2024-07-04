3bd86ce85be46973a3cec9978d2305edb624f434
Jarhatz <rajathrao1001@gmail.com>
2024-07-04 14:55:59 -0700

    clean up push

--- src/components/ParticlesComponent.tsx
+++ src/components/ParticlesComponent.tsx
@@ -2,10 +2,7 @@ import "../index.css";
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { type Container } from "@tsparticles/engine";
-// import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-// import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+import { loadFull } from "tsparticles"; // https://github.com/tsparticles/react
 import options from "./ParticlesConfig";
 import "./ParticlesComponent.css";
 
