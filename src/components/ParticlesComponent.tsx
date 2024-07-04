9ff5a1ef4a381bde6805caae8b522a76bc2a6280
Jarhatz <rajathrao1001@gmail.com>
2024-07-04 14:44:48 -0700

    fixed tsparticles

--- src/components/ParticlesComponent.tsx
+++ src/components/ParticlesComponent.tsx
@@ -2,7 +2,10 @@ import "../index.css";
 import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { type Container } from "@tsparticles/engine";
-import { loadSlim } from "@tsparticles/slim";
+// import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+// import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
 import options from "./ParticlesConfig";
 import "./ParticlesComponent.css";
 
@@ -11,7 +14,7 @@ function ParticlesComponent() {
 
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      await loadSlim(engine);
+      await loadFull(engine);
     }).then(() => {
       setInit(true);
     });
@@ -30,6 +33,7 @@ function ParticlesComponent() {
       />
     );
   } else {
+    console.log("Failed.");
     return <></>;
   }
 }
